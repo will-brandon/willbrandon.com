d85320bd9f6053db77113d9ccd447af8c7508143
Will Brandon <will17brandon@icloud.com>
2023-08-16 15:50:08 -0400

    Sync

--- src/index.tsx
+++ src/index.tsx
@@ -12,10 +12,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import {parseCommandTokens} from "./util/StringUtil";
 
 // Find the root HTML root div element and make it the React root element.
 const htmlRootElement = document.getElementById('root') as HTMLDivElement
 const reactRootElement = ReactDOM.createRoot(htmlRootElement);
+
+console.log(parseCommandTokens(""));
+console.log(parseCommandTokens("a b c"));
+console.log(parseCommandTokens("\"This 'is\" a test!!!\t!"));
+console.log(parseCommandTokens("  'hello  ' world \"\" ''     I want to \"TEST \" strings and escapes."));
+//console.log(parseCommandTokens("   yup, that \" SHOULD' \" DO it"));
 
 // Render the app in the React root element.
 reactRootElement.render(
