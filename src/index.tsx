2703845af8cd18f2db53b09d716a24afcee0390c
Will Brandon <will17brandon@icloud.com>
2023-09-09 17:42:16 -0400

    Cleaned up

--- src/index.tsx
+++ src/index.tsx
@@ -12,7 +12,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import CommandParser from "./terminal/shell/CommandParser";
 
 // Find the root HTML root div element and make it the React root element.
 const htmlRootElement = document.getElementById('root') as HTMLDivElement
